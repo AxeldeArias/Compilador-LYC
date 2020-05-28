51a104dfab63d71cd0f250a48c98feab8f64994b
axel dearias <axeldearias@gmail.com>
2020-05-28 00:18:42 -0300

    delete generadores

--- src/Generadores/A_Sintactico.bat
+++
@@ -1,5 +0,0 @@
-SET PATH=%JAVA_HOME%;%PATH%
-SET CLASSPATH=%JAVA_HOME%;
-cd src\Analizadores
-java -jar "..\..\lib\jcup\java-cup-11b.jar" -parser analisis_sintactico -symbols Simbolos A_Sintactico.cup
-pause
